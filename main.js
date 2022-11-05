ed4c2b353abfe747513a3de54baf62a6c48dc9b6
Shuang Li <catherinelishuang1997@gmail.com>
2022-11-05 12:06:13 +0200

    change the style of the map

--- main.js
+++ main.js
@@ -3,15 +3,19 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY2F0aGVyaW5lbGlpaWlpIiwiYSI6ImNsYTMzZGtsbDBuO
 const map = new mapboxgl.Map({
 container: 'map',
 // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-style: 'mapbox://styles/catherineliiiii/cla3o8rmi000214l74oo3zint',
+style: 'mapbox://styles/catherineliiiii/cla3qmm8200fl14nxi500yc65',
 center: [24.945831,60.192059],
 zoom: 6
+
 });
  
 // Add the control to the map.
 map.addControl(
 new MapboxGeocoder({
 accessToken: mapboxgl.accessToken,
+marker:{
+    color:"green"
+},
 mapboxgl: mapboxgl
 })
 );
