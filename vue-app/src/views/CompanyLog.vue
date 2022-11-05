004a378d1e294a7ed7d8c342ff4d722655611fed
Shuang Li <catherinelishuang1997@gmail.com>
2022-11-06 01:51:59 +0200

    change the formatof the title for company page

--- vue-app/src/views/CompanyLog.vue
+++ vue-app/src/views/CompanyLog.vue
@@ -1,9 +1,7 @@
 <script setup>
 import CompanyLog from "../components/CompanyLog.vue";
-import get_environment from '../environment.js';
+import get_environment from "../environment.js";
 </script>
-
-
 
 <script>
 export default {
@@ -12,7 +10,7 @@ export default {
     CompanyLog,
   },
   data() {
-    this.explore_options()
+    this.explore_options();
     return {
       error_feedback: "",
       query_result_approved: [],
@@ -22,12 +20,12 @@ export default {
   },
   methods: {
     async explore_options() {
-      const backend_url = get_environment().backend_url
+      const backend_url = get_environment().backend_url;
       const url = `${backend_url}/api/v1/order`;
       const data = await this._get_request(url);
-      this.query_result_approved = data.filter(d => d.status == 'APPROVED')
-      this.query_result_denied = data.filter(d => d.status == 'DENIED')
-      this.query_result_pending = data.filter(d => d.status == 'PENDING')
+      this.query_result_approved = data.filter((d) => d.status == "APPROVED");
+      this.query_result_denied = data.filter((d) => d.status == "DENIED");
+      this.query_result_pending = data.filter((d) => d.status == "PENDING");
     },
     async _get_request(url) {
       try {
@@ -44,22 +42,33 @@ export default {
 </script>
 
 <template>
-  <div class="container">
-    <h1>Purchase Log</h1>
+  <main class="container mt-5" role="main">
+    <div class="container">
+      <section class="jumbotron text-center">
+        <div class="container">
+          <h1 class="jumbotron-heading">Purchase Log</h1>
+          <p class="lead text-muted">
+            Something short and leading about the collection below—its contents,
+            the creator, etc. Make it short and sweet, but not too short so
+            folks don't simply skip over it entirely.
+          </p>
+        </div>
+      </section>
 
-    <div class="mt-5">
-      <h1>Approved</h1>
-      <CompanyLog :query_result="query_result_approved" />
-    </div>
+      <div class="mt-5">
+        <h4>Approved</h4>
+        <CompanyLog :query_result="query_result_approved" />
+      </div>
 
-    <div class="mt-5">
-      <h1>Denied</h1>
-      <CompanyLog :query_result="query_result_denied" />
-    </div>
+      <div class="mt-5">
+        <h4>Denied</h4>
+        <CompanyLog :query_result="query_result_denied" />
+      </div>
 
-    <div class="mt-5">
-      <h1>Pending</h1>
-      <CompanyLog :query_result="query_result_pending" />
+      <div class="mt-5">
+        <h4>Pending</h4>
+        <CompanyLog :query_result="query_result_pending" />
+      </div>
     </div>
-  </div>
+  </main>
 </template>
