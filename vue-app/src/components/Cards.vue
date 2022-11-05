590b85d220d0f4ae649fb71e6538fea5df03f438
jagger-en <japman2400@gmail.com>
2022-11-05 21:52:16 +0200

    Show product data in catalog

--- vue-app/src/components/Cards.vue
+++ vue-app/src/components/Cards.vue
@@ -1,9 +1,26 @@
+<script setup>
+import get_environment from "../environment.js";
+defineProps({
+  product: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
 <template>
   <div class="card">
-    <img class="card-img-top" src="..." alt="{{ ShortDescription }}" />
+    <!-- <img class="card-img-top" src="@/assets/images/foo.png" alt="{{ ShortDescription }}" /> -->
     <div class="card-body">
-      <h5 class="card-title">ProductName</h5>
-      <p class="card-text">LongDescription</p>
+      <h5 class="card-title">{{ product.name }}</h5>
+      <p class="card-text">
+        {{ product.full_info }}
+        <ul>
+          <li>{{ product.price }} {{ product.sales_unit }}</li>
+          <li>{{ product.weight_kg }} kg</li>
+          <li>{{ product.total_volume_m3 }} m3</li>
+        </ul>
+      </p>
       <a href="#" class="btn btn-success">Add to basket</a>
     </div>
   </div>
--- vue-app/src/views/ProductCatalog.vue
+++ vue-app/src/views/ProductCatalog.vue
@@ -1,5 +1,41 @@
 <script setup>
 import Cards from "../components/Cards.vue";
+import get_environment from '../environment.js';
+</script>
+
+
+<script>
+export default {
+  name: "product-catalog-view",
+  components: {
+    Cards,
+  },
+  data() {
+    this.explore_options()
+    return {
+      error_feedback: "",
+      query_result: [],
+    };
+  },
+  methods: {
+    async explore_options() {
+      const backend_url = get_environment().backend_url
+      const url = `${backend_url}/api/v1/product`;
+      const data = await this._get_request(url);
+      this.query_result = data
+    },
+    async _get_request(url) {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        this.error_feedback = "Failed to fetch data from backend.";
+        return [];
+      }
+    },
+  },
+};
 </script>
 
 <template>
@@ -8,9 +44,11 @@ import Cards from "../components/Cards.vue";
 
     <div class="mt-5">
       <div class="card-columns">
-        <Cards />
-        <Cards />
-        <Cards />
+        <div
+        v-for="row in query_result" :key="row.id"
+        >
+          <Cards :product="row" />
+        </div>
       </div>
     </div>
   </div>
