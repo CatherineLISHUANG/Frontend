01da3072660f692ace2fbe4d5891c32b7681b981
Shuang Li <catherinelishuang1997@gmail.com>
2022-11-05 17:06:40 +0200

    add the map

--- vue-app/src/views/HomeView.vue
+++ vue-app/src/views/HomeView.vue
@@ -1,3 +1,36 @@
+<script>
+
+
+export default {
+  mounted() {
+    console.log('all good')
+
+    //show the map
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoiY2F0aGVyaW5lbGlpaWlpIiwiYSI6ImNsYTMzZGtsbDBuOWIzbm1vY3RveWRtNDYifQ.hbr58j4aPCWcEk_-a80fEQ";
+    const map = new mapboxgl.Map({
+      container: "map",
+      // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+      style: "mapbox://styles/catherineliiiii/cla3qmm8200fl14nxi500yc65",
+      center: [24.945831, 60.192059],
+      zoom: 6,
+    });
+
+    // Add the control to the map.
+    map.addControl(
+      new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        marker: {
+          color: "green",
+        },
+        mapboxgl: mapboxgl,
+      })
+    );
+
+
+  }
+}
+  </script>
 <template>
   <main class="container mt-5" role="main">
       <!--Map-->
@@ -5,4 +38,6 @@
         <div class="col-12" id="map"></div>
       </div>
     </main>
+ 
 </template>
+ 
