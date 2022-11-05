136a06800228558a5e72d82c86d645c3d7d05062
Shuang Li <catherinelishuang1997@gmail.com>
2022-11-05 11:31:33 +0200

    edit the map with search function

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 //show the map
-L.mapbox.accessToken ="pk.eyJ1IjoiY2F0aGVyaW5lbGlpaWlpIiwiYSI6ImNsYTMzZGtsbDBuOWIzbm1vY3RveWRtNDYifQ.hbr58j4aPCWcEk_-a80fEQ";
-var map = L.mapbox
-.map("map")
-.setView([60.192059, 24.945831], 6)
-.addLayer(L.mapbox.styleLayer("mapbox://styles/catherineliiiii/cla3o8rmi000214l74oo3zint"));
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2F0aGVyaW5lbGlpaWlpIiwiYSI6ImNsYTMzZGtsbDBuOWIzbm1vY3RveWRtNDYifQ.hbr58j4aPCWcEk_-a80fEQ';
+const map = new mapboxgl.Map({
+container: 'map',
+// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+style: 'mapbox://styles/catherineliiiii/cla3o8rmi000214l74oo3zint',
+center: [24.945831,60.192059],
+zoom: 6
+});
+ 
+// Add the control to the map.
+map.addControl(
+new MapboxGeocoder({
+accessToken: mapboxgl.accessToken,
+mapboxgl: mapboxgl
+})
+);
+
